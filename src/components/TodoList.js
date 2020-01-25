e6317cdcaa244e12a42ba8a965f696528a123a5a
zohaib younis <zabi.jarral08@gmail.com>
2020-01-25 21:47:02 +0100

    Toggle Todo and Delete Todo Has been created using Redux hooks

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,36 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 
 
 
 export default () => {
+    const todos = useSelector((state) => state.todos)
+    const dispatch = useDispatch();
     return (
-        <p>Todos.....</p>
+        <ul className="todo-list">
+            {
+                todos.map(todo => (
+                    <li key={todo.id}>
+                        <input type="checkbox"
+                            checked={todo.complete}
+                            onChange={() => dispatch({ type: 'TOGGLE_TODO', payload: todo.id })}
+                        />
+                        <span className={todo.complete ? 'complete' : null}>{todo.name}</span>
+                        <span className='delete-button' style={{
+                            backgroundColor: "red",
+                            marginLeft: "1rem",
+                            cursor: "pointer"
+                        }} onClick={() => dispatch({ type: 'DELETE_TODO', payload: todo.id })}>X</span>
+
+                    </li>
+
+
+                )
+
+                )
+            }
+        </ul>
     )
 
 }
